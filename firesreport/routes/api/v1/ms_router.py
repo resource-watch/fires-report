e1938725d66eb71ed0dff33ac0d352151bdc89d5
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-02-14 13:27:12 +0100

    endpoint finished

--- firesreport/routes/api/v1/ms_router.py
+++ firesreport/routes/api/v1/ms_router.py
@@ -15,7 +15,7 @@ from firesreport.utils.http import request_to_microservice, request_to_url
 
 @endpoints.route('/firesreport', methods=['GET'])
 def do_reports():
-    """Do Fires Report"""
+    '''Do Fires Report'''
     logging.info('Doing fires report')
 
     islands = request.args.get('islands', None)
@@ -24,14 +24,14 @@ def do_reports():
 
     if (not islands and not provinces) or not period:
         return jsonify({'errors': [{
-            'status': "400",
+            'status': '400',
             'title': 'Islands or provinces (and period) should be set'
             }]
         }), 400
 
     if len(period.split(',')) < 2:
         return jsonify({'errors': [{
-            'status': "400",
+            'status': '400',
             'title': 'Period needs 2 arguments'
             }]
         }), 400
@@ -40,21 +40,48 @@ def do_reports():
     period_to = period.split(',')[1]
 
     try:
-        valid_from  = datetime.datetime.strptime(period_from, "%Y-%m-%d")
-        valid_to = datetime.datetime.strptime(period_to, "%Y-%m-%d")
+        datetime.datetime.strptime(period_from, '%Y-%m-%d')
+        datetime.datetime.strptime(period_to, '%Y-%m-%d')
     except ValueError:
         return jsonify({'errors': [{
-            'status': "400",
+            'status': '400',
             'title': 'Not valid dates'
             }]
         }), 400
 
-    config = {
-        'url': 'http://gis-potico.wri.org/arcgis/rest/services/Fires/FIRMS_ASEAN/MapServer/0',
-        'method': 'GET',
+    if islands:
+        column = 'ISLAND'
+        values = islands.split(',')
+    else:
+        column = 'PROVINCE'
+        values = provinces.split(',')
+
+    #values = map(lambda x: '\'' + str(x) + '\'', values)
+    values = "'"+ "','".join(values)+"'"
+
+    logging.debug(values)
+
+    payload = {
+        'f':'json',
+        'spatialRelationship':'esriSpatialRelIntersects',
+        'where':column +' in ('+values+') ACQ_DATE >= '+ period_from +' AND ACQ_DATE <= ' + period_to,
+        'returnGeometry':False,
+        'groupByFieldsForStatistics':['ACQ_DATE'],
+        'orderByFields':['ACQ_DATE ASC'],
+        'outStatistics':[{
+            'onStatisticField':'ACQ_DATE',
+            'outStatisticFieldName':'Count',
+            'statisticType':'count'
+        }]
     }
-    response = request_to_url(config)
 
-    return jsonify({'data': 'hello'}), 200
+    try:
+        response = requests.get('http://gis-potico.wri.org/arcgis/rest/services/Fires/FIRMS_ASEAN/MapServer/0', params=payload)
+    except Error:
+        return jsonify({'errors': [{
+            'status': '500',
+            'title': 'Service unavailable'
+            }]
+        }), 500
 
-#{"spatialRelationship":"esriSpatialRelIntersects","where":"ISLAND in ('Java','Kalimantan','Lesser Sunda','Maluku','Papua','Sulawesi','Sumatra')","returnGeometry":false,"groupByFieldsForStatistics":["ACQ_DATE"],"orderByFields":["ACQ_DATE ASC"],"outStatistics":[{"onStatisticField":"ACQ_DATE","outStatisticFieldName":"Count","statisticType":"count"}]}
+    return jsonify(response.json()), 200
